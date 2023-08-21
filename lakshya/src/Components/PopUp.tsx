4e8ed224ea3977a740d3d075c60a5f7ae8d5920e
LakshyaG34 <lakshyagoyal34@gmail.com>
2023-08-21 14:05:36 +0530

    Third commit

--- lakshya/src/Components/PopUp.tsx
+++ lakshya/src/Components/PopUp.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import './style.css'
 function Popup({ country, city }: { country: string; city: string }) {
     const popupText = `Your country is ${country} and city is ${city}`;
   
     return (
       <div>
-        <p className="text-red">{popupText}</p>
+        <p className="text-white">{popupText}</p>
       </div>
     );
   }
